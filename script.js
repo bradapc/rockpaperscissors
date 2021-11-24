a5433b27223a3f46bc76b12986fdc946c6e46ba5
Brad <brad.cocar@gmail.com>
2021-11-24 15:11:12 -0800

    Added computer rolls

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+let computerChoice;
+
+function computerPlay() {
+    x = Math.floor(Math.random() * 3);
+    console.log(x);
+    if(x === 0) {
+        computerChoice = "Rock";
+    } else if(x === 1) {
+        computerChoice = "Paper";
+    } else if (x === 2) {
+        computerChoice = "Scissors";
+    }
+}
