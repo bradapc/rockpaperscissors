14266055e2f561faf732117ede18d2f818eb291a
Brad <brad.cocar@gmail.com>
2021-11-24 17:20:25 -0800

    Remade the player and computer choosing so there are no global variables, made the rock paper scissors game computed

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-let computerChoice;
-
 function computerPlay() {
+    let computerChoice;
     x = Math.floor(Math.random() * 3);
-    console.log(x);
     if(x === 0) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if(x === 1) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else if (x === 2) {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
+    }
+    return computerChoice;
+}
+
+function playerChoose() {
+    let playerChoice;
+    do {
+        playerChoice = prompt("Rock, paper, or scissors?");
+        playerChoice = playerChoice.toLowerCase();
+    } while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
+    return playerChoice;
+}
+
+function rockPaperScissors(playerChoice, computerChoice) {
+    console.log(playerChoice);
+    console.log(computerChoice);
+    if(playerChoice === "rock") {
+        if(computerChoice === "rock") {
+            return "Tie game! Rock vs. rock";
+        } else if(computerChoice === "scissors") {
+            return "You win! Rock beats scissors!";
+        } else if(computerChoice === "paper") {
+            return "You lose! Paper beats rock!";
+        }
+    } else if(playerChoice === "paper") {
+        if(computerChoice === "rock") {
+            return "You win! Paper beats rock!";
+        } else if(computerChoice === "paper") {
+            return "Tie game! Paper vs. paper";
+        } else if(computerChoice === "scissors") {
+            return "You lose! Scissors beats paper!";
+        }
+    } else if(playerChoice === "scissors") {
+        if(computerChoice === "rock") {
+            return "You lose! Rock beats scissors!";
+        } else if(computerChoice === "paper") {
+            return "You win! Scissors beats paper!";
+        } else if(computerChoice === "scissors") {
+            return "Tie game! Scissors vs. scissors";
+        }
     }
 }
 
-let playerChoice;
-function playerChoose() {
-    playerChoice = prompt("Rock, paper, or scissors?");
-    if(playerChoice.toLowerCase() == "rock") {
-        return;
-    } else if(playerChoice.toLowerCase() == "paper") {
-        return;
-    } else if(playerChoice.toLowerCase() == "scissors") {
-        return;
-    } else {
-        playerChoose();
-    }
-}
+console.log(rockPaperScissors(playerChoose(), computerPlay()));
