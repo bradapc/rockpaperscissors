aee7be29c97653e96790c88f49ebff3838f4dba2
Brad <brad.cocar@gmail.com>
2021-11-26 15:10:59 -0800

    Added UI and made code functional for it

--- script.js
+++ script.js
@@ -11,60 +11,85 @@ function computerPlay() {
     return computerChoice;
 }
 
-function playerChoose() {
-    let playerChoice;
-    do {
-        playerChoice = prompt("Rock, paper, or scissors?");
-        playerChoice = playerChoice.toLowerCase();
-    } while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
-    return playerChoice;
+function playerChoose(playerSelection) {
+    return playerSelection;
 }
 
+const outputText = document.querySelector('.textresult');
 function rockPaperScissors(playerChoice, computerChoice) {
     if(playerChoice === "rock") {
         if(computerChoice === "rock") {
-            console.log("Tie game! Rock vs. rock");
+            outputText.textContent = "Tie game! Rock vs. rock";
         } else if(computerChoice === "scissors") {
-            console.log("You win! Rock beats scissors!");
-            return playerScore++;
+            outputText.textContent = "You win! Rock beats scissors!";
+            playerScore++;
         } else if(computerChoice === "paper") {
-            console.log("You lose! Paper beats rock!");
-            return computerScore++;
+            outputText.textContent = "You lose! Paper beats rock!";
+            computerScore++;
         }
     } else if(playerChoice === "paper") {
         if(computerChoice === "rock") {
-            console.log("You win! Paper beats rock!");
-            return playerScore++;
+            outputText.textContent = "You win! Paper beats rock!";
+            playerScore++;
         } else if(computerChoice === "paper") {
-            console.log("Tie game! Paper vs. paper");
+            outputText.textContent = "Tie game! Paper vs. paper";
         } else if(computerChoice === "scissors") {
-            console.log("You lose! Scissors beats paper!");
-            return computerScore++;
+            outputText.textContent = "You lose! Scissors beats paper!";
+            computerScore++;
         }
     } else if(playerChoice === "scissors") {
         if(computerChoice === "rock") {
-            console.log("You lose! Rock beats scissors!");
-            return computerScore++;
+            outputText.textContent = "You lose! Rock beats scissors!";
+            computerScore++;
         } else if(computerChoice === "paper") {
-            console.log("You win! Scissors beats paper!");
-            return playerScore++;
+            outputText.textContent = "You win! Scissors beats paper!";
+            playerScore++;
         } else if(computerChoice === "scissors") {
-            console.log("Tie game! Scissors vs. scissors");
+            outputText.textContent = "Tie game! Scissors vs. scissors";
         }
     }
+    game();
 }
+
+const btn = document.querySelectorAll('.playbutton');
+btn.forEach(addListen => addListen.addEventListener('click', () => {
+    let id = this.id;
+    rockPaperScissors(playerChoose(addListen.id), computerPlay())
+}));
 let playerScore = 0;
 let computerScore = 0;
+const scoreText = document.querySelector('.runningscore');
 function game() {
-    do {
-        rockPaperScissors(playerChoose(), computerPlay());
-        console.log(`Score: ${playerScore} You ${computerScore} Computer`);
-    } while(playerScore < 5 && computerScore < 5);
+    scoreText.textContent = `Score: ${playerScore} You | ${computerScore} Computer`;
     if(playerScore == 5) {
-        console.log(`You won! Final score:\n ${playerScore} You ${computerScore} Computer`);
+        outputText.textContent = `You won!`;
+        outputText.style.color = 'green';
+        scoreText.textContent = `Final score:\n ${playerScore} You | ${computerScore} Computer`
+        endGame();
     } else if(computerScore == 5) {
-        console.log(`You lost. Final score:\n ${playerScore} You ${computerScore} Computer`);
+        outputText.textContent = `You lost.`;
+        outputText.style.color = 'red';
+        scoreText.textContent = `Final score:\n ${playerScore} You | ${computerScore} Computer`
+        endGame();
     }
 }
 
-game();
+function endGame() {
+    btn.forEach(disableButton => disableButton.disabled = true);
+    const restartButton = document.querySelector('.restart');
+    restartButton.style.display = 'block';
+    restartButton.addEventListener('click', () => {
+        restartGame();
+    });
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    outputText.textContent = 'Choose an option';
+    outputText.style = 'initial';
+    scoreText.textContent = 'Score:';
+    const restartButton = document.querySelector('.restart');
+    restartButton.style.display = 'none';
+    btn.forEach(enableButton => enableButton.disabled = false);
+}
